1e35583ccaa3528f14eb442a4f09da73578b1939
realSZ27 <99378252+realSZ27@users.noreply.github.com>
2024-05-29 19:41:50 -0500

    Change back linux version to old one :cry:

--- mods.sh
+++ mods.sh
@@ -4,97 +4,13 @@ number=1
 
 lethalCompanyDir='/run/media/deck/8d6b7a8c-3438-4d30-a279-03ac9405b35e/steamapps/common/Lethal Company'
 
-#!/bin/bash
-
-read -p "What drive is your Lethal Company install on? (press enter for /mnt/c/ & MAKE SURE TO CAPITALIZE) " lethalCompanyDirI
-read -p "Do you want to:
-1. Update
-2. Uninstall
-3. Install
-4. Disable/Enable Mods
-" action
-
-number=1
-
-enableDisableMods() {
-    folderItems=$(ls -A "$lethalCompanyDir/BepInEx/plugins")
-    
-    if [ -z "$folderItems" ]; then
-        echo -e "\e[34mEnabling mods...\e[0m"
-        mv "$lethalCompanyDir/BepInEx/disabled_mods/"* "$lethalCompanyDir/BepInEx/plugins"
-        rm -rf "$lethalCompanyDir/BepInEx/disabled_mods"
-        echo -e "\e[32mFinished enabling mods...\e[0m"
-        exit
-    else
-        echo -e "\e[34mDisabling mods...\e[0m"
-        mkdir -p "$lethalCompanyDir/BepInEx/disabled_mods"
-        mv "$lethalCompanyDir/BepInEx/plugins/"* "$lethalCompanyDir/BepInEx/disabled_mods"
-        echo -e "\e[32mFinished disabling mods\e[0m"
-        exit
-    fi
-}
-
-checkBepInEx() {
-    if [ ! -d "$lethalCompanyDir/BepInEx" ]; then
-        echo -e "\e[34mInstalling/Reinstalling BepInEx...\e[0m"
-        mkdir -p ./temp
-        downloadFilePath="./temp/BepInEx.zip"
-        
-        curl -L -o "$downloadFilePath" "https://thunderstore.io/package/download/BepInEx/BepInExPack/5.4.2100/"
-        
-        unzip -o "$downloadFilePath" -d ./temp/
-        
-        cp -r ./temp/BepInExPack/* "$lethalCompanyDir"
-        
-        rm -rf ./temp
-        echo -e "\e[32mFinished installing BepInEx\e[0m"
-    fi
-}
-
-uninstallMods() {
-    read -p "Are you sure you want to uninstall BepInEx and all related files? (y/n) " confirmation
-    if [[ "$confirmation" == "y" || "$confirmation" == "Y" || -z "$confirmation" ]]; then
-        rm -rf "$lethalCompanyDir/BepInEx"
-        rm -f "$lethalCompanyDir/winhttp.dll"
-        rm -f "$lethalCompanyDir/doorstop_config.ini"
-        exit
-    else
-        exit
-    fi
-}
-
-updateMods() {
-    rm -rf "$lethalCompanyDir/BepInEx"
-    rm -f "$lethalCompanyDir/winhttp.dll"
-    rm -f "$lethalCompanyDir/doorstop_config.ini"
-    checkBepInEx
-}
-
-case "$action" in
-    1)
-        updateMods
-        ;;
-    2)
-        echo -e "\e[34mUninstalling Mods...\e[0m"
-        uninstallMods
-        echo -e "\e[32mSuccess\e[0m"
-        ;;
-    4)
-        enableDisableMods
-        ;;
-esac
-
-echo -e "\e[34mInstalling Mods...\e[0m"
-
-checkBepInEx
-
 # Array of links
 downloadLinks=(
     "https://thunderstore.io/package/download/notnotnotswipez/MoreCompany/1.9.1/"
-    "https://thunderstore.io/package/download/RickArg/Helmet_Cameras/2.1.5/"
+    "https://thunderstore.io/package/download/RickArg/Helmet_Cameras/2.1.5/",
     "https://thunderstore.io/package/download/RugbugRedfern/Skinwalkers/5.0.0/"
     "https://thunderstore.io/package/download/Norman/Lightsabers/1.1.2/"
-    "https://thunderstore.io/package/download/Evaisa/LethalLib/0.16.0/"
+	"https://thunderstore.io/package/download/Evaisa/LethalLib/0.16.0/"
     "https://thunderstore.io/package/download/MoonJuice/RubberChicken/1.0.3/"
     "https://thunderstore.io/package/download/Kittenji/Groan_Tube_Scrap/1.0.1/"
     "https://thunderstore.io/package/download/Nemotoad/PortalTurrets/1.0.0/"
@@ -103,8 +19,8 @@ downloadLinks=(
     "https://thunderstore.io/package/download/TryToDiveMe/Bikini_Bottom/2.1.0/"
     "https://thunderstore.io/package/download/jockie/LethalExpansionCore/1.3.15/"
     "https://thunderstore.io/package/download/sfDesat/Celest/1.1.2/"
-    "https://thunderstore.io/package/download/sfDesat/ViewExtension/1.2.0/"
-    "https://thunderstore.io/package/download/IAmBatby/LethalLevelLoader/1.2.4/"
+	"https://thunderstore.io/package/download/sfDesat/ViewExtension/1.2.0/"
+	"https://thunderstore.io/package/download/IAmBatby/LethalLevelLoader/1.2.4/"
     "https://thunderstore.io/package/download/Kittenji/Maxwell_ScrapItem/1.0.1/"
     "https://thunderstore.io/package/download/blink9803/DimmingFlashlights/1.0.1/"
     "https://thunderstore.io/package/download/PanHouse/LethalClunk/1.1.1/"
@@ -122,20 +38,31 @@ downloadLinks=(
     "https://thunderstore.io/package/download/FlipMods/ReservedWalkieSlot/2.0.5/"
     "https://thunderstore.io/package/download/FlipMods/ReservedFlashlightSlot/2.0.5/"
     "https://thunderstore.io/package/download/PXC/ShipLootPlus/1.1.0/"
-    "https://thunderstore.io/package/download/AinaVT/LethalConfig/1.4.2/"
+	"https://thunderstore.io/package/download/AinaVT/LethalConfig/1.4.2/"
 )
 
+# Temporary folder for downloading and extracting contents
+tempFolder="./temp"
+
 for link in "${downloadLinks[@]}"; do
-    fileName=$number
-    mkdir -p ./temp
-    downloadFilePath="./temp/${fileName}.zip"
-    
+    # Extract the file name from the URI
+    fileName="$number"
+
+    # Create a temp folder
+    mkdir -p "$tempFolder"
+
+    # Specify the local path where you want to save the downloaded file
+    downloadFilePath="$tempFolder/${fileName}.zip"
+
+    # Download the file using curl
     curl -L -o "$downloadFilePath" "$link"
-    
+
+    # Unzip the contents to the specified temporary destination folder
     unzip -o "$downloadFilePath" -d "$lethalCompanyDir"
-    
-    rm -rf ./temp
-    
+
+    # Remove the temporary download folder
+    rm -rf "$tempFolder"
+
     number=$((number + 1))
 done
 
@@ -143,14 +70,14 @@ done
 filesToExclude=("doorstop_config.ini" "Lethal Company.exe" "nvngx_dlss.dll" "NVUnityPlugin.dll" "UnityCrashHandler64.exe" "UnityPlayer.dll" "winhttp.dll" "Lethal Company_Data" "MonoBleedingEdge")
 
 # Get all files in the source folder
-files=$(ls -A "$lethalCompanyDir")
+files=("$lethalCompanyDir"/*)
 
 # Loop through each file and move it if not in the exclusion list
-for file in $files; do
-    if [[ ! " ${filesToExclude[@]} " =~ " $(basename $file) " ]]; then
-        mv "$lethalCompanyDir/$file" "$lethalCompanyDir/BepInEx/plugins"
+for file in "${files[@]}"; do
+    fileName=$(basename "$file")
+    if [[ ! " ${filesToExclude[@]} " =~ " $fileName " ]]; then
+        mv "$file" "${lethalCompanyDir}/BepInEx/plugins" -f
     fi
 done
 
-echo -e "\e[32mFinished installing mods\e[0m"
-read -p "Press Enter to continue..."
+read -p "Press Enter to exit..."
